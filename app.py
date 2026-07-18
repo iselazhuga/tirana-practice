ff337d9f38c6dd7b4bc1af3bbbef5d084cb07175
isela <iselazhuga@gmail.com>
2026-07-19 01:30:09 +0200

    display apartment data in the app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", city="Tirana")
+    df = pd.read_csv("data/apartments.csv")
+    listings = df.to_dict(orient="records")
+    return render_template("index.html", listings=listings)
 
 if __name__ == "__main__":
     app.run(debug=True)
