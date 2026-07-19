16e6ba5df2e82098f6d7ba1376255b7a60407605
isela <iselazhuga@gmail.com>
2026-07-19 21:28:09 +0200

    add neighborhood coordinates

--- app.py
+++ app.py
@@ -3,6 +3,25 @@ import pandas as pd
 import joblib
 
 app = Flask(__name__)
+NEIGHBORHOOD_COORDS = {
+    "Bllok": (41.3231, 19.8172),
+    "Don Bosko": (41.3105, 19.7959),
+    "Kombinat": (41.2990, 19.7775),
+    "Fresk": (41.3400, 19.8100),
+    "Yzberisht": (41.3350, 19.7600),
+    "Selitë": (41.3450, 19.8250),
+    "Astir": (41.3600, 19.8400),
+    "Ali Demi": (41.3150, 19.8300),
+    "Laprakë": (41.3300, 19.7700),
+    "Njësia 5": (41.3250, 19.8150),
+    "21 Dhjetori": (41.3280, 19.8200),
+    "Liqeni i Thatë": (41.3500, 19.8000),
+}
+
+TIRANA_CENTER = (41.3275, 19.8187)
+
+def get_coordinates(neighborhood):
+    return NEIGHBORHOOD_COORDS.get(neighborhood, TIRANA_CENTER)
 
 @app.route("/")
 def home():
